455665d40e6c64b7c042d18341a900d2ed4d3659
NoumanAmir657 <noumanamir453@gmail.com>
2023-05-07 13:41:08 +0500

    added thread pools

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ ./server.cpp hashtable.cpp avl.cpp zset.cpp heap.cpp -o server
+g++ ./server.cpp hashtable.cpp avl.cpp zset.cpp heap.cpp thread_pool.cpp -o server
 g++ ./client.cpp -o client
